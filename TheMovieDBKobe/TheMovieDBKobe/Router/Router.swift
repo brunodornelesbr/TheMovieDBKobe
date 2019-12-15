ad74b9c48af41b94a1d20ce9230bebe326658ee2
Bruno Dorneles <>
2019-12-14 23:05:07 -0300

    reorganizing file structure

---
+++ TheMovieDBKobe/TheMovieDBKobe/Router/Router.swift
@@ -0,0 +1,13 @@
+//
+//  Router.swift
+//  TheMovieDBKobe
+//
+//  Created by Bruno Dorneles on 14/12/19.
+//  Copyright © 2019 Bruno Dorneles. All rights reserved.
+//
+
+import UIKit
+
+class Router {
+
+}
