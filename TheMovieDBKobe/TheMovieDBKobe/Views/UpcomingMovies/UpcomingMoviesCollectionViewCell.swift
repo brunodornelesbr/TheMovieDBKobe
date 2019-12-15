52596e5c1fb0e08400c2fafb4d1d6cc957e80f54
Bruno Dorneles <>
2019-12-15 07:44:00 -0300

    created collectionviewcell for upcoming movies screen

---
+++ TheMovieDBKobe/TheMovieDBKobe/Views/UpcomingMovies/UpcomingMoviesCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  UpcomingMoviesCollectionViewCell.swift
+//  TheMovieDBKobe
+//
+//  Created by Bruno Dorneles on 15/12/19.
+//  Copyright © 2019 Bruno Dorneles. All rights reserved.
+//
+
+import UIKit
+
+class UpcomingMoviesCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var posterLabel: UILabel!
+    
+}
